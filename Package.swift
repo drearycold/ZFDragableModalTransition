a32a9c83cae0785e7849ef726e13b12854e05123
Peter <peterlee@peter-xcp-debian.lan>
2021-01-26 16:51:47 +0800

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ZFDragableModalTransition",
-            path: "ZFDragableModalTransition",
-            dependencies: []),
+            path: "ZFDragableModalTransition"
+        ),
     ]
 )
 
